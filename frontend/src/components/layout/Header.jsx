6af21e11cf7332e742c438ba094031ea45dd1ae5
Denis R <rozenkod312@gmail.com>
2025-11-03 22:33:57 +0500

    \Hide header completely"

--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -98,7 +98,7 @@ const Header = () => {
     };
 
     return (
-        <header className="flex h-16 items-center justify-end gap-2 sm:gap-4 px-3 sm:px-6 lg:h-[70px] bg-transparent">
+        <header className="hidden">
             {/* Кнопка интеграций (для всех авторизованных пользователей, включая гостей) */}
             {isAuthenticated && (
                 <div className="relative integrations-menu">
